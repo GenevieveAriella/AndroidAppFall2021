ea93cee9884f82c999e2115ef1d21e3e758417f2
qle <quangle270799@gmail.com>
2021-10-17 10:29:17 -0400

    Added onclick method to link landing page to menu page

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -4,6 +4,7 @@ import static android.widget.Toast.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -16,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public void nextScreen(View view) {
-//        makeText(getApplicationContext(),"Next Screen", LENGTH_SHORT).show();
-//    }
+    /**
+    The purpose of this public method is to link our views/pages together
+     **/
+    public void signInButtonClick(View view){
+        // Intent takes in 2 arguments, the context and the page you want to link to. In this case,
+        // I want to link to menuPage. Next step is to go to the sign in button and loads this method
+        // into the onCLick attribute
+        Intent intent = new Intent(this, menuPage.class);
+        startActivity(intent);
+    }
+
 }
