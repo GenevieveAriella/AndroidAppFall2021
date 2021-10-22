7c98ce4b66f01f250618189029e96348f7e82394
qle <quangle270799@gmail.com>
2021-10-22 14:06:33 -0400

    temp commit

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
