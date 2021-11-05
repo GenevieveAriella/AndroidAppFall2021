9626bd5d0c4ab752cee7695636b68014c8aa585c
Blue Arevalo <arevaloh@vcu.edu>
2021-11-05 15:20:00 -0400

    Added basework for spotify api in Main activity

--- app/src/main/java/com/example/learningandroid/MainActivity.java
+++ app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // tbd
+    }
+    private void connected() {
+        // Tbd
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // tbd done
+    }
+    private static final String CLIENT_ID = "14fecf12fef34758ad734d913a897ec4";
+    private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
 //    public void nextScreen(View view) {
 //        makeText(getApplicationContext(),"Next Screen", LENGTH_SHORT).show();
