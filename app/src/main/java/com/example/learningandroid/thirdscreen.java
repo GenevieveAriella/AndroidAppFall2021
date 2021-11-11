24d8a5bc1a61c01ac3d3b9e0e075e31a6fc0388f
GenevieveAriella <dooleymm@vcu.edu>
2021-11-10 22:31:16 -0500

    need to work on scroll view

--- app/src/main/java/com/example/learningandroid/thirdscreen.java
+++ app/src/main/java/com/example/learningandroid/thirdscreen.java
@@ -5,21 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class thirdscreen extends AppCompatActivity {
-
+    CheckBox cb1;
+    CheckBox cb2;
+    CheckBox cb3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thirdscreen);
-
-    }
+        cb1 = findViewById(R.id.checkBox1);
+        cb2 = findViewById(R.id.checkBox2);
+        cb3 = findViewById(R.id.checkBox3);
+            }
     public void goBacktoMenu(View view) {
         // Intent takes in 2 arguments, the context and the page you want to link to. In this case,
         // I want to link to menuPage. Next step is to go to the sign in button and loads this method
         // into the onCLick attribute
         Intent intent = new Intent(this, menuPage.class);
         startActivity(intent);
+        cb1 = findViewById(R.id.checkBox1);
+        cb2 = findViewById(R.id.checkBox2);
+        cb3 = findViewById(R.id.checkBox3);
     }
+    public void unselectAll(View view) {
+        if (cb1.isChecked()) {
+            cb1.setChecked(false);
+        }
+        if (cb2.isChecked()) {
+            cb2.setChecked(false);
+        }
+        if (cb3.isChecked()) {
+            cb3.setChecked(false);
+        }
 
+    }
+    public void selectAll(View view) {
+        if (cb1.isChecked() == false) {
+            cb1.setChecked(true);
+        }
+        if (cb2.isChecked() == false) {
+            cb2.setChecked(true);
+        }
+        if (cb3.isChecked() == false) {
+            cb3.setChecked(true);
+        }
+    }
 }
