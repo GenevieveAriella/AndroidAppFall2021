c7a70d32cf5a9a9b387b72dede46ce703be45c08
GenevieveAriella <dooleymm@vcu.edu>
2021-11-23 16:33:53 -0500

    revert commit

--- app/src/main/java/com/example/learningandroid/thirdscreen.java
+++ app/src/main/java/com/example/learningandroid/thirdscreen.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class thirdscreen extends AppCompatActivity {
     CheckBox cb1;
     CheckBox cb2;
     CheckBox cb3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,8 @@ public class thirdscreen extends AppCompatActivity {
         cb1 = findViewById(R.id.checkBox1);
         cb2 = findViewById(R.id.checkBox2);
         cb3 = findViewById(R.id.checkBox3);
-            }
+    }
+
     public void goBacktoMenu(View view) {
         // Intent takes in 2 arguments, the context and the page you want to link to. In this case,
         // I want to link to menuPage. Next step is to go to the sign in button and loads this method
@@ -29,6 +33,7 @@ public class thirdscreen extends AppCompatActivity {
         cb2 = findViewById(R.id.checkBox2);
         cb3 = findViewById(R.id.checkBox3);
     }
+
     public void unselectAll(View view) {
         if (cb1.isChecked()) {
             cb1.setChecked(false);
@@ -41,6 +46,7 @@ public class thirdscreen extends AppCompatActivity {
         }
 
     }
+
     public void selectAll(View view) {
         if (cb1.isChecked() == false) {
             cb1.setChecked(true);
@@ -52,4 +58,18 @@ public class thirdscreen extends AppCompatActivity {
             cb3.setChecked(true);
         }
     }
+/*    ViewGroup layout= (ViewGroup) findViewById(R.id.linearlayout);
+    public void switchToTrue(View v) {
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View w = layout.getChildAt(i);
+            if (v instanceof CheckBox) {
+                if (!((CheckBox) v).isChecked()) {
+                    ((CheckBox) v).setChecked(true);
+                }
+            }
+        }
+
+
+    }*/
+
 }
