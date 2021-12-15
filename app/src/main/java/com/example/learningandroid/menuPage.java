13b4524ca9d9c821096c8bc8d8e879ba35f22e57
Ash-Malai18 <56330219+Ash-Malai18@users.noreply.github.com>
2021-12-15 02:42:46 -0500

    Update local.properties
    created variable for message for toast message

--- app/src/main/java/com/example/learningandroid/menuPage.java
+++ app/src/main/java/com/example/learningandroid/menuPage.java
@@ -5,15 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class menuPage extends AppCompatActivity {
+
+    Button start_saving;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_page);
 
+        start_saving=(Button)findViewById(R.id.start_saving);
+        start_saving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(menuPage.this, "You only have discover weekly for 52 possible to be saved", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 
     /**
      The purpose of this public method is to link our menu page to the landing page together
